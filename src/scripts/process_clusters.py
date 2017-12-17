09ace68f0c1c16c03562c17194a91c4b9e65c72f
Izaak van Dongen <izaak.van.dongen@gmail.com>
2017-12-17 22:32:01 +0000

    process into sorted order by position

--- src/scripts/process_clusters.py
+++ src/scripts/process_clusters.py
@@ -12,7 +12,7 @@ def get_args():
 def inform(row):
     words = [node.split("_")[0] for node in row.split()]
     perts = [ind for ind, row in enumerate(zip(*words)) if len(set(row)) != 1]
-    return "{{{}}}: {}".format(" ".join(map(str, perts)), " ".join(row.split()))
+    return "{{{}}}: {}".format(" ".join(map(str, perts)), " ".join(sorted(row.split(), key=lambda x: int(x.split("_")[1]))))
 
 if __name__ == "__main__":
     args = get_args()
